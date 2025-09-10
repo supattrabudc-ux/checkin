64adf0179d57cd0a6886611c0002ab58f003e397
supattrabudc-ux <supattra.budc@cpird.in.th>
2025-09-10 16:17:27 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbx5Xop6Bzse9PvYnX4aYuPGaqlL7SR1o5fkFQKOxQnfCMbCQd78etqy_CGxWxiVuVVH/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzHVfYkelc5F3yBRJeatAdeN9Bf1qdqcU_M8wPT3lQuhlbYBj47sy4V05GAQAFjTtE/exec';
 //LIFF_ID
     const LIFF_ID = '2008087994-M1z7vQQY'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087994-oR05Y88V'; // LIFF ID history
     const LIFF_REGISTER = '2008087994-zam5oRR3'; // LIFF ID register
+
 
