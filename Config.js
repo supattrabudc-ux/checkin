82fd1b570820396f0ff0e170500550efb9dbf2c9
supattrabudc-ux <supattra.budc@cpird.in.th>
2025-09-10 15:24:42 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,7 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
 //LIFF_ID
-    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
-    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
+    const LIFF_ID = '2008087994-M1z7vQQY'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008087994-oR05Y88V'; // LIFF ID history
+    const LIFF_REGISTER = '2008087994-zam5oRR3'; // LIFF ID register
+
